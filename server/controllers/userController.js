4e7145d201830ca32ad1222034a931726588fcd8
Md Shadman Mahfuz Raafi <156221826+Raafi8900@users.noreply.github.com>
2025-04-16 23:44:47 +0600

    code
    code updated

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -57,4 +57,16 @@ const loginUser = async (req,res)=>{
 
 };
 
-export {registerUser, loginUser};
+const userCredits = async (req, res) =>{
+  try {
+    const {userId} = req.body
+
+    const user = await userModel.findById(userId)
+    res.json({success: true, credit: user.creditBalance, user:{name: user.name}})
+  } catch (error) {
+    console.log(error.mesaage)
+    res.json({success:false, message:error.message})
+  }
+}
+
+export {registerUser, loginUser, userCredits};
