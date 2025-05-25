0657449a926e7604681006b1aa48898e75e439b1
Md Shadman Mahfuz Raafi <156221826+Raafi8900@users.noreply.github.com>
2025-05-25 12:55:43 +0600

    updated
    updated

--- server/server.js
+++ server/server.js
@@ -10,7 +10,10 @@ const PORT = process.env.PORT || 4000
 const app = express()
 
 app.use(express.json())
-app.use(cors()) 
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
 await connectDB()
 
 
